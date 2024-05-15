92ae1f088650596d7443d1ba683ef0b3b9361724
liuwang <1183862787@qq.com>
2024-05-15 22:04:31 +0800

    upernet r50 mosaic4x configfile

---
+++ configs/upernet/upernet_r50_2xb8-80k_agrivisoin-512x512-mosaic4x.py
@@ -0,0 +1,96 @@
+_base_ = [
+    '../_base_/models/upernet_r50.py', '../_base_/datasets/agriculture_vision.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+
+crop_size = (512, 512)
+data_preprocessor = dict(
+    size=crop_size,
+    mean=[111.46, 113.90, 112.23, 118.30],
+    std=[43.75, 41.29, 41.72, 46.56],
+    bgr_to_rgb=False,
+)
+data_root = 'data/2024-CVPR-Agriculture-Vision/supervised/Agriculture-Vision-2021-MMSeg-RGBN'
+train_pipeline = [
+    dict(type='LoadTifImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(512, 512),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(512, 512), cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5, direction=['horizontal']),
+    dict(type='RandomFlip', prob=0.5, direction=['vertical']),
+    dict(type='RandomRotate90', prob=0.5, degree=90),
+    dict(type='PhotoMetricDistortionTif'),
+    dict(type='PackSegInputs')
+]
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    dataset=dict(
+        data_root=data_root,
+        pipeline=train_pipeline,
+        data_prefix=dict(
+            img_path='img_dir/train_val_mosaic',
+            seg_map_path='ann_dir/train_val_mosaic'),
+    )
+)
+val_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    dataset=dict(
+        data_root=data_root
+    )
+)
+test_dataloader = val_dataloader
+
+model = dict(
+    data_preprocessor=data_preprocessor,
+    backbone=dict(in_channels=4),
+    decode_head=dict(
+        num_classes=9,
+        loss_decode=dict(
+            type='ACWLoss',
+            loss_weight=1.0
+        )
+    ),
+    auxiliary_head=dict(
+        type='FCNHead',
+        num_classes=9,
+        loss_decode=dict(
+            type='ACWLoss',
+            loss_weight=0.4
+        )
+    ),
+)
+
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=80000,
+        by_epoch=False,
+    )
+]
+
+default_hooks = dict(
+    checkpoint=dict(by_epoch=False, interval=8000, max_keep_ckpts=2,
+                    save_last=True, save_best=['mIoU'], type='CheckpointHook'))
