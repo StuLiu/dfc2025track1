7aed9699b8c2a0e829457b5cf00531bc8319d8b2
liuwang <1183862787@qq.com>
2025-02-10 17:20:01 +0800

    saving 2 ckpts

--- configs/segformer/02_08_segformer_mit-b3_4xb2-40k_sarseg-768x768_official.py
+++ configs/segformer/02_08_segformer_mit-b3_4xb2-40k_sarseg-768x768_official.py
@@ -58,5 +58,5 @@ test_dataloader = dict(batch_size=1, num_workers=4)
 
 train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
 default_hooks = dict(
-    checkpoint=dict(by_epoch=False, interval=4000, max_keep_ckpts=1,
+    checkpoint=dict(by_epoch=False, interval=4000, max_keep_ckpts=2,
                     save_last=True, save_best=['mIoU'], type='CheckpointHook'))
