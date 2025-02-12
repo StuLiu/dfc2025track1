2ff31a7e26b8cf0645ecaf3c7fc22c33a58c9039
liuwang <1183862787@qq.com>
2025-02-12 23:22:39 +0800

    fix bug when slide testing on a small-size image.

--- mmseg/models/segmentors/encoder_decoder.py
+++ mmseg/models/segmentors/encoder_decoder.py
@@ -243,6 +243,74 @@ class EncoderDecoder(BaseSegmentor):
         """Inference by sliding-window with overlap.
 
         If h_crop > h_img or w_crop > w_img, the small patch will be used to
+        decode with padding.
+        Args:
+            inputs (tensor): the tensor should have a shape NxCxHxW,
+                which contains all images in the batch.
+            batch_img_metas (List[dict]): List of image metainfo where each may
+                also contain: 'img_shape', 'scale_factor', 'flip', 'img_path',
+                'ori_shape', and 'pad_shape'.
+                For details on the values of these keys see
+                `mmseg/datasets/pipelines/formatting.py:PackSegInputs`.
+
+        Returns:
+            Tensor: The segmentation results, seg_logits from model of each
+                input image.
+        """
+
+        h_stride, w_stride = self.test_cfg.stride
+        h_crop, w_crop = self.test_cfg.crop_size
+        batch_size, _, h_img, w_img = inputs.size()
+        out_channels = self.out_channels
+        h_grids = max(h_img - h_crop + h_stride - 1, 0) // h_stride + 1
+        w_grids = max(w_img - w_crop + w_stride - 1, 0) // w_stride + 1
+        preds = inputs.new_zeros((batch_size, out_channels, h_img, w_img))
+        count_mat = inputs.new_zeros((batch_size, 1, h_img, w_img))
+        for h_idx in range(h_grids):
+            for w_idx in range(w_grids):
+                y1 = h_idx * h_stride
+                x1 = w_idx * w_stride
+                y2 = min(y1 + h_crop, h_img)
+                x2 = min(x1 + w_crop, w_img)
+                y1 = max(y2 - h_crop, 0)
+                x1 = max(x2 - w_crop, 0)
+                crop_img = inputs[:, :, y1:y2, x1:x2]
+
+                # padding to the crop size if the crop_img is smaller than expected one.
+                def pad_image(img, target_size):
+                    """Pad an image up to the target size."""
+                    h_missing = target_size[0] - img.shape[2]  # h
+                    w_missing = target_size[1] - img.shape[3]  # w
+                    if h_missing > 0 or w_missing > 0:
+                        padded_img = F.pad(img, (0, w_missing, 0, h_missing), 'constant', 0)
+                        return padded_img
+                    else:
+                        return img
+                crop_img = pad_image(crop_img, (h_crop, w_crop))
+
+                # change the image shape to patch shape
+                batch_img_metas[0]['img_shape'] = crop_img.shape[2:]
+                # the output of encode_decode is seg logits tensor map
+                # with shape [N, C, H, W]
+                crop_seg_logit = self.encode_decode(crop_img, batch_img_metas)
+
+                # cropping out the origin img
+                crop_seg_logit = crop_seg_logit[:, :, :y2 - y1, :x2 - x1]
+
+                preds += F.pad(crop_seg_logit,
+                               (int(x1), int(preds.shape[3] - x2), int(y1),
+                                int(preds.shape[2] - y2)))
+
+                count_mat[:, :, y1:y2, x1:x2] += 1
+        assert (count_mat == 0).sum() == 0
+        seg_logits = preds / count_mat
+
+        return seg_logits
+
+    def slide_inference_without_pad(self, inputs: Tensor, batch_img_metas: List[dict]) -> Tensor:
+        """Inference by sliding-window with overlap.
+
+        If h_crop > h_img or w_crop > w_img, the small patch will be used to
         decode without padding.
 
         Args:
