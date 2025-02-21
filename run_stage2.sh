1bd810284634f4d916c1d6d653bba8841cb3bd97
liuwang <1183862787@qq.com>
2025-02-21 14:18:15 +0800

    add configs

--- run_stage2.sh
+++ run_stage2.sh
@@ -1,11 +1,11 @@
 
 
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-n_gpu=4
+export CUDA_VISIBLE_DEVICES=2,3
+n_gpu=2
 
-name='02_14_segformer_mit-b5_4xb2-160k_sarseg-768x768_interv1_ignore255_gce-lovasz'
+name='02_21_uda_segformer_mit-b3_2xb4-40k_sarseg-768x768_official_dacs_ce_th0.968'
 bash tools/dist_train.sh configs/_dfc_stage2/${name}.py ${n_gpu}
-bash tools/dist_test.sh configs/_dfc_stage2/${name}.py work_dirs/${name}/iter_160000.pth ${n_gpu} \
+bash tools/dist_test.sh configs/_dfc_stage2/${name}.py work_dirs/${name}/iter_40000.pth ${n_gpu} \
   --out submits/_dfc_stage2/${name}_tta --tta
 cd submits/_dfc_stage2/${name}_tta || exit
 zip -r ../mmseg_${name}_tta.zip ./*.png
