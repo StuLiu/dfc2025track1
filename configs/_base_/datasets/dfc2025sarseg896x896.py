03f984334d7d1b70ace344ec0f993053ada93cc7
liuwang <1183862787@qq.com>
2025-02-27 23:44:34 +0800

    add configs

--- configs/_base_/datasets/dfc2025sarseg896x896.py
+++ configs/_base_/datasets/dfc2025sarseg896x896.py
@@ -1,7 +1,7 @@
 # dataset settings
 dataset_type = 'DFC2025SarSegDataset'
 data_root = '/home/liuwang/liuwang_data/documents/datasets/seg/challenges/DFC2025Track1'
-crop_size = (1024, 1024)
+crop_size = (896, 896)
 train_pipeline = [
     dict(type='LoadTifImageFromFileV4'),
     dict(type='LoadTifAnnotationsV2'),
