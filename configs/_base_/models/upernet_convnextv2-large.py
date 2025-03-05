32040f3f5dceb0d3898a90442d825d0232fa7860
liuwang <1183862787@qq.com>
2025-03-05 20:26:03 +0800

    add configs for upernet_convnextv2-large

---
+++ configs/_base_/models/upernet_convnextv2-large.py
@@ -0,0 +1,64 @@
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+custom_imports = dict(imports='mmpretrain.models', allow_failed_imports=False)
+checkpoint_file = ('https://download.openmmlab.com/mmclassification/v0/convnext-v2/'
+                   'convnext-v2-large_fcmae-in21k-pre_3rdparty_in1k-384px_20230104-9139a1f3.pth')
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=False,
+    pad_val=0,
+    seg_pad_val=255)
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    pretrained=None,
+    backbone=dict(
+        type='mmpretrain.ConvNeXt',
+        arch='large',
+        in_channels=3,
+        use_grn=True,
+        drop_path_rate=0.0,
+        layer_scale_init_value=0.,
+        stem_patch_size=4,
+        norm_cfg=dict(type='LN2d', eps=1e-6),
+        act_cfg=dict(type='GELU'),
+        linear_pw_conv=True,
+        frozen_stages=0,
+        gap_before_final_norm=False,
+        with_cp=False,
+        out_indices=[0, 1, 2, 3],
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=checkpoint_file,
+            prefix='backbone.'
+        )
+    ),
+    decode_head=dict(
+        type='UPerHead',
+        in_channels=[192, 384, 768, 1536],
+        in_index=[0, 1, 2, 3],
+        pool_scales=(1, 2, 3, 6),
+        channels=512,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=768,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
